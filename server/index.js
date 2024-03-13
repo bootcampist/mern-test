e58bcd43740b43805286646bbed68a4dd1c8ce7e
bootcampist <152117886+bootcampist@users.noreply.github.com>
2024-03-13 15:21:01 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors(
 ));
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
